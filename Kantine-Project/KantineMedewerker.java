7af20313368a8855b3a976b65f2893fef0dfae7c
Michaelew <43989931+Michaelew@users.noreply.github.com>
2019-06-11 12:08:12 +0200

    medewerker en student

--- Kantine-Project/KantineMedewerker.java
+++ Kantine-Project/KantineMedewerker.java
@@ -1,5 +1,9 @@
 
 public class KantineMedewerker extends Persoon {
+	
+	public int medewerkernummer;
+	public boolean kassabeschikbaar;
+	
 
 	public KantineMedewerker(int BSN, String voornaam, String achternaam, String geboortedatum, char geslacht) {
 		super(BSN, voornaam, achternaam, geboortedatum, geslacht);
