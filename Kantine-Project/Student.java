538b26e56d1c0f792675583ea9f6f01c5f902402
Michaelew <43989931+Michaelew@users.noreply.github.com>
2019-06-11 12:18:32 +0200

    kantinemedewerker en student get set

--- Kantine-Project/Student.java
+++ Kantine-Project/Student.java
@@ -1,16 +1,38 @@
 
 public class Student extends Persoon {
 	
-	public int studentnummer
-	public String studierichting
+	public int studentnummer;
+	public String studierichting;
 
 	public Student(int BSN, String voornaam, String achternaam, String geboortedatum, char geslacht) {
 		super(BSN, voornaam, achternaam, geboortedatum, geslacht);
 		// TODO Auto-generated constructor stub
+		
+		this.studentnummer = studentnummer;
+		this.studierichting = studierichting;
+		
 	}
 
 	public Student() {
 		// TODO Auto-generated constructor stub
+		this.studentnummer = 0;
+		this.studierichting = "default";
+	}
+	
+	public void setStudentnummer(int studentnummer) {
+		this.studentnummer = studentnummer;
+	}
+	
+	public void setStudierichting() {
+		this.studierichting = studierichting;
 	}
 
+	
+	public int getStudentnummer() {
+		return studentnummer;
+	}
+	
+	public String getStudierichting() {
+		return studierichting;
+	}
 }
